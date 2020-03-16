fb65abc03139f92a5f17b9ebd5b8f961e33f5a0c
Mark <glmarkgl4@gmail.com>
2020-03-16 14:52:42 +0300

    backend - start socket io

--- backend/server.js
+++ backend/server.js
@@ -1,30 +1,26 @@
-const express = require('express');
-const cors = require('cors');
 const mongoose = require("mongoose");
-const bodyParser = require('body-parser');
+const app = require('express')();
+const http = require('http').createServer(app);
+const io = require('socket.io');
+const socket = io(http);
 const db = require('./app/config/db');
 const auth = require('./app/auth');
-
 const port = 8090;
-const app = express();
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(bodyParser.json());
-app.use(cors());
-
-mongoose.connect(db.url, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false,
-    useCreateIndex: true
+//
+// mongoose.connect(db.url, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//     useFindAndModify: false,
+//     useCreateIndex: true
+// });
+// database ? console.log("Db connected successfully") : console.log("Error connecting db");
+// const database = mongoose.connection;
+socket.on('connection', (socket) => {
+    console.log('user connected');
+    socket.on('disconnect', () => {
+        console.log('Disconnected!');
+    })
 });
-const database = mongoose.connection;
-database ? console.log("Db connected successfully") : console.log("Error connecting db");
-
-const privateApiRoutes = require('./app/route/private-api-routes');
-const publicApiRoutes = require('./app/route/public-api-routes');
-app.use('/', publicApiRoutes);
-app.use(auth.isAuthorized);
-app.use('/', privateApiRoutes);
-app.listen(port, () => {
-    console.log("Running RestHub on port " + port);
+http.listen(port, (socketConnectOpts) => {
+    console.log('Connected to port: ' + port)
 });
